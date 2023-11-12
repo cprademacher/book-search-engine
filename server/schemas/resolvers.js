e78c9847d6cfa05b7cdb959bf17a54bd65be8851
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-11-12 09:18:24 -0600

    Corrected schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,39 +2,39 @@ const { User, Book } = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-      users: async () => {
-        return User.find().populate("thoughts");
-      },
-      user: async (parent, { username }) => {
-        return User.findOne({ username }).populate("thoughts");
-      },
+  Query: {
+    users: async () => {
+      return User.find();
     },
-  
-    Mutation: {
-      addUser: async (parent, { username, email, password }) => {
-        const user = await User.create({ username, email, password });
-        const token = signToken(user);
-        return { token, user };
-      },
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-  
-        if (!user) {
-          throw AuthenticationError;
-        }
-  
-        const correctPw = await user.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw AuthenticationError;
-        }
-  
-        const token = signToken(user);
-  
-        return { token, user };
-      },
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
     },
-  };
-  
-  module.exports = resolvers;
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
